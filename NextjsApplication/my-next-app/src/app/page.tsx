1016fb692280662b9648fed43805b59639054f16
SafdarAliShahPC <programmersafdar@live.com>
2024-03-11 11:59:52 +0500

    next-app-testing

---
+++ NextjsApplication/my-next-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <h1 className="text-green-400 text-lg font-bold">Learning Tailwind CSS</h1>
+      </body>
+    </html>
+  )
+}
