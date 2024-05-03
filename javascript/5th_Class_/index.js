6c79506c83dca55f25d536e699c29d8cce53fa8c
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-03 19:31:34 +0500

    updated

---
+++ javascript/5th_Class_/index.js
@@ -0,0 +1,34 @@
+//Class 5th in JavaScript -> Objects
+//  Note: In JS an object is entity having some properties
+//  and mehtods also known as => (function)
+// const student = {
+//     name: 'Safdar',
+//     age: 29,
+//     subject: ['CS', 'Maths', 'PS'],
+//     marks() {
+//         //we use this keyword to access the object properties
+//         return `${this.subject[0]} is 90%, ${this.subject[1]} is 80%, ${this.subject[2]} is 75%`
+//     },
+// }
+// This below marks method is the same as above mars method, just a different way of
+//writing methods inside object.
+//console.log(student)
+// student = ['safdar', 'ali', 'shah']
+// class Human {
+//     eat() {
+//         console.log("Eating")
+//     }
+//     pray() {
+//         console.log("Praying")
+//     }
+//     sleep() {
+//         console.log("Sleeping")
+//     }
+//     run() {
+//         console.log("Running")
+//     }
+// }
+
+// let H = new Human()
+
+//  Constructors is called when object is initialized
